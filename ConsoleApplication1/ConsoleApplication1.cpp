3859f4b6ab4b0ee4852b7d9962e0e5883aba0309
NORAHC <NORAHC@DESKTOP-MCBN1EA>
2022-09-15 00:08:20 +0200

    CPP-learn

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,16 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int some_number;
+    cout << "Hello World!\n";
+    cin >> some_number;
+
+    system("pause");
+    return 0;
+
+}
