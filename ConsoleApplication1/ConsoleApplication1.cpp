3d051ce6d913abc2d3a90b8d5bd3d02f07c122ee
NORAHC <NORAHC@DESKTOP-MCBN1EA>
2022-09-15 03:43:51 +0200

    Added Comments

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,14 +1,27 @@
+/*
+If i add some code i need to commit it first
 
+
+*/
 
 #include <iostream>
-
+//Names given to parts of code to help reduce naming conflicts ===== std ===== is a C++ 'standart' namespace 
 using namespace std;
+// ==== :: ==== is a scope resolution operator
 
+//All cpp files needs main one main function with or without arguments
 int main()
 {
+    //Simple string variable  char type
     string some_name;
+
+    //Stream out 
     cout << "Hello World!\n";
+
+    //Stream in (to write something in console)
     cin >> some_name;
+
+    //Standart bool UE logic (if/elif/else)
     if (some_name == "Karl")
     {
         cout << "How did you know that?"<<endl;
@@ -17,8 +30,8 @@ int main()
     {
         cout << "Hello " + some_name << endl;
     }
+    
     system("pause");
-
     return 0;
 
 }
